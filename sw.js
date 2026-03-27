4ad6edaf1681170afc32a0e76292c5e4e5b17db3
phongphiphat prombut <piko38261@gmail.com>
2026-03-27 11:49:12 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-const CACHE_NAME = 'hive-pwa-cache-v1';
+const CACHE_NAME = 'hive-pwa-cache-v2';
 
-// 📦 รายชื่อไฟล์ที่ต้องการให้แอปจดจำไว้ในเครื่อง (โหลดไวขึ้น)
 const urlsToCache = [
   './',
   './index.html',
@@ -10,18 +9,16 @@ const urlsToCache = [
   './manifest.json'
 ];
 
-// ⚙️ ติดตั้ง Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('เปิดใช้งาน Cache สำเร็จ');
+        console.log('เปิดใช้งาน Cache HIVE V2 สำเร็จ');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// 🔄 ระบบดึงข้อมูล: ถ้าไม่มีเน็ต ให้ดึงจาก Cache มาแสดงแทน
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
@@ -32,14 +29,13 @@ self.addEventListener('fetch', event => {
   );
 });
 
-// 🧹 ล้าง Cache เก่าทิ้งเวลาอัปเดตเวอร์ชัน (v2, v3...)
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
+            return caches.delete(cacheName); // ลบ DOSH อันเก่าทิ้ง
           }
         })
       );
